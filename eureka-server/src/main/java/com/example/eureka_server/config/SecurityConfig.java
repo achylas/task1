bf6ecbdd558104d31d1463ffe3d4c18fb5ea681c
Achylas <inshk17@gmail.com>
2025-04-30 15:56:44 +0500

    task1

---
+++ eureka-server/src/main/java/com/example/eureka_server/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.eureka_server.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/eureka/**").authenticated()
+                .anyRequest().permitAll()
+            )
+            .httpBasic();
+
+        return http.build();
+    }
+}
+
+    
